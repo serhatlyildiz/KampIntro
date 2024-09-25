57f5ed7d96f46a99e7d792dbb9892ee3ca9fd115
Serhat Yıldız <serhat.yildiz2@gazi.edu.tr>
2024-09-25 14:49:09 +0300

    24 eylül 2024 ders sonu

---
+++ Oop2/TuzelMusteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop2
+{
+    //Coorporate
+    //miras inheritance
+    class TuzelMusteri:Musteri
+    {
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
